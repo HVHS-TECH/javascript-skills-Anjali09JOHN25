438b3f6dc7a6e49e0ed742107c7d78087922f32e
Anjali09JOHN25 <24127aj@hvhs.school.nz>
2026-04-30 00:55:40 +0000

    Apr 30, 2026, 12:55 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,20 @@
-/* Paste your code from task 1 here */
+/****************************
+Introduction to JavaScript
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Hello, my name is Anjali")
+
+// Variables
+var pocketMoney = 3;
+console.log (pocketMoney);
+
+/****************************
+Main Code
+****************************/
+
+
+
+/****************************
+Functions
+****************************/
+
