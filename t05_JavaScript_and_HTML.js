2281409dbe8d8430cca1723708ee159c473ce40d
Anjali09JOHN25 <24127aj@hvhs.school.nz>
2026-05-04 21:57:43 +0000

    May 4, 2026, 9:57 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -5,6 +5,8 @@ Introduction to JavaScript
 console.log("Running t05_JavaScript_and_HTML.js")
 console.log("Hello, my name is Anjali")
 
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
 // Variables
 var pocketMoney = 4;
 console.log (pocketMoney);
@@ -21,18 +23,17 @@ console.log (currentYear);
 /****************************
 Main Code
 ****************************/
+OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML = "<p>Hello" + userName + "</p>";
+OUTPUT.innerHTML = "<p>As of" +  currentYear  + "you are" + userAge + "years old</p>";
+OUTPUT.innerHTML = "<p>In 10 years you will be", userAge + 10 + "years old</p>";
+
 console.log ("Hi" + userName)
 console.log ("As of" +  currentYear  + "you are" + userAge + "years old")
 console.log ("In 10 years you will be", userAge + 10 + "years old" )
 console.log ("You have" + pocketMoney + "dollars")
 console.log ("You spend half of your money, now you have", pocketMoney / 2 +"dollars")
 console.log ("Then you get 3 dollars, now you have", pocketMoney + 3 +"dollars")
-
-const OUTPUT = document.getElementById("JavaScriptOutput");
-OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
-OUTPUT.innerHTML = "<p>Hello" + userName + "</p>";
-OUTPUT.innerHTML = "<p> As of" +  currentYear  + "you are" + userAge + "years old"</p>";
-OUTPUT.innerHTML = "<p>In 10 years you will be", userAge + 10 + "years old</p>";
 
 
 /****************************
