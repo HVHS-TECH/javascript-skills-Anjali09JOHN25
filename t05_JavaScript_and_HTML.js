1fbbb5ec2d77a7894711890bd34b1a105326c760
Anjali09JOHN25 <24127aj@hvhs.school.nz>
2026-05-04 21:21:29 +0000

    May 4, 2026, 9:21 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,35 @@
 /* Paste your code from task 3, Maths here */
+/****************************
+Introduction to JavaScript
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Hello, my name is Anjali")
+
+// Variables
+var pocketMoney = 4;
+console.log (pocketMoney);
+
+var userName = "Anjali";
+console.log (userName);
+
+var userAge = 15;
+console.log (userAge);
+
+var currentYear = 2026;
+console.log (currentYear);
+
+/****************************
+Main Code
+****************************/
+console.log ("Hi" + userName)
+console.log ("As of" +  currentYear  + "you are" + userAge + "years old")
+console.log ("In 10 years you will be", userAge + 10 + "years old" )
+console.log ("You have" + pocketMoney + "dollars")
+console.log ("You spend half of your money, now you have", pocketMoney / 2 +"dollars")
+console.log ("Then you get 3 dollars, now you have", pocketMoney + 3 +"dollars")
+
+
+/****************************
+Functions
+****************************/
+
