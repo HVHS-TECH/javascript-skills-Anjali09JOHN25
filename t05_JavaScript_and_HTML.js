dbbca7a22c7a990aa00686c9b19feb1682c21f4f
Anjali09JOHN25 <24127aj@hvhs.school.nz>
2026-05-04 21:23:29 +0000

    May 4, 2026, 9:23 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -2,7 +2,7 @@
 /****************************
 Introduction to JavaScript
 ****************************/
-console.log("Running t01_introduction.js")
+console.log("Running t05_JavaScript_and_HTML.js")
 console.log("Hello, my name is Anjali")
 
 // Variables
