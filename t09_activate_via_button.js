5b4b821e769f6861b8a4302209a82fda8ced3325
Anjali09JOHN25 <24127aj@hvhs.school.nz>
2026-05-14 00:51:36 +0000

    May 14, 2026, 12:51 AM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,54 @@
 /* Paste your code from task 7 here*/
+/****************************
+Introduction to JavaScript
+****************************/
+console.log("Running t06_functions.js")
+console.log("Hello, my name is Anjali")
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+
+// Variables
+var pocketMoney = 4;
+console.log (pocketMoney);
+
+var userName = "Anjali";
+console.log (userName);
+
+var userAge = 15;
+console.log (userAge);
+
+var currentYear = 2026;
+console.log (currentYear);
+
+/****************************
+Functions
+****************************/
+function welcome(){
+  OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
+}
+
+
+welcome()
+
+/****************************
+Main Code
+****************************/
+
+OUTPUT.innerHTML += "<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
+OUTPUT.innerHTML += "<p>As of " + currentYear + "you are " + userAge + "years old </p>";
+OUTPUT.innerHTML += "<p> You have" + pocketMoney + "dollars</p>";
+OUTPUT.innerHTML += "<img src='Dog.jpeg' alt='A cute dog'>";
+
+
+console.log ("Hi" + userName)
+console.log ("As of" +  currentYear  + "you are" + userAge + "years old")
+console.log ("In 10 years you will be", userAge + '10'  + "years old" )
+console.log ("You have" + pocketMoney + "dollars")
+console.log ("You spend half of your money, now you have", pocketMoney / 2 +"dollars")
+console.log ("Then you get 3 dollars, now you have", pocketMoney + 3 +"dollars")
+
+
+/****************************
+Functions
+****************************/
