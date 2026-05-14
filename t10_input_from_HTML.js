7f9395ac465ce1b4dd7cb97ee61f57624b9b73da
Anjali09JOHN25 <24127aj@hvhs.school.nz>
2026-05-14 01:14:39 +0000

    May 14, 2026, 1:14 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -34,6 +34,12 @@ function welcome(){
   OUTPUT.innerHTML += "<p>Welcome to the shop</p>";
 }
 
+function getFormInput(){
+  const NAME_FIELD = document.getElementById("nameField");
+  let userName = NAME_FIELD.value;
+  OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+}
+
 
 welcome()
 
